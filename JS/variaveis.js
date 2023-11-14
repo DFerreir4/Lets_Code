94005ff063e944b3c1454f9f70f2422068ab223d
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-11-14 19:08:24 -0300

    variáveis

---
+++ JS/variaveis.js
@@ -0,0 +1,28 @@
+//Variáveis
+
+let idade = 26 //Criando a primeira variavel
+
+console.log(idade)
+
+//Tipos de variáveis
+
+idade = 30              //number
+let numero = 30         //number
+let altura = 1.77       //number
+let nome = 'Diego'      //string (texto)
+let estudando = true    //Boleano (true ou false)
+
+console.log(numero, typeof numero)
+console.log(altura, typeof altura)
+console.log(nome, typeof nome)
+console.log(estudando, typeof estudando)
+
+let outra;
+console.log(outra, typeof outra)
+
+let nula = null
+console.log(nula, typeof nula)
+
+var variavel = 30 //Não utilizar o var
+
+const constante = 100 // não pode alterar por ser uma constante
