900825a4b4f36e4f635346bd2015648d0c0d0727
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-11-14 19:19:01 -0300

    Operadores Aritméticos

---
+++ JS/operadores_aritmeticos.js
@@ -0,0 +1,23 @@
+//Operadores Aritméticos
+
+let n1 = 20;
+let n2 = 2;
+
+console.log(`Soma = ${n1+n2}`)
+console.log(`Subtração = ${n1-n2}`)
+console.log(`Multiplicação = ${n1*n2}`)
+console.log(`Divisão = ${n1/n2}`)
+console.log(`Exponenciação = ${n1**n2}`)
+console.log(`Resto de divisão = ${n1%n2}`)
+
+//Procedência de operadores
+
+console.log((3+7)*2)
+console.log((10+3+8)/3)
+
+//Biblioteca Math
+
+console.log(Math.sqrt(16))
+
+
+
