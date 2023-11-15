658a11ce042f79be637e0d28b3e84f37ff8aff7c
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-11-14 23:56:08 -0300

    Estados e eventos

--- React/meu-projeto-react/src/App.jsx
+++ React/meu-projeto-react/src/App.jsx
@@ -1,25 +1,12 @@
-import MeuComponente from "./components/MeuComponente"
+import Meucontador from "./Meucontador"
 
 function App() {
   
 
   return (
     <div>
-     <h1>Olá Mundo, React!</h1>
-     <MeuComponente/>
-     <MeuBotao conteudo='me clique'/>
-
-     <MeuBotao conteudo='depois aqui'/>
-
-     <MeuBotao conteudo='e por fim aqui'/>
+     <Meucontador/>
     </div>
-  )
-}
-
-function MeuBotao(props) {
-
-  return(
-    <button>{props.conteudo}</button>
   )
 }
 
---
+++ React/meu-projeto-react/src/Meucontador.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+export default function Meucontador() {
+
+    console.log("REnderizando meu contador...")
+
+    const [contador, setContador] = useState(0)
+    
+
+    function aumentar() {
+        setContador(contador + 1)
+    }
+
+    return(
+        <div>
+            <h1>Meu Contador</h1>
+            <h3>{contador}</h3>
+            <button onClick={aumentar}>
+                aumentar
+            </button>
+        </div>
+
+    )
+}
+
