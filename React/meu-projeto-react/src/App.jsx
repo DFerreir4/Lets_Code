cf98a7b0365102c0606135e72ed2fd8eb9421ede
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-11-14 23:35:51 -0300

    Components e props

--- React/meu-projeto-react/src/App.jsx
+++ React/meu-projeto-react/src/App.jsx
@@ -1,9 +1,28 @@
+import MeuComponente from "./components/MeuComponente"
+
 function App() {
   
 
   return (
-    <h1>Olá Mundo, React!</h1>
+    <div>
+     <h1>Olá Mundo, React!</h1>
+     <MeuComponente/>
+     <MeuBotao conteudo='me clique'/>
+
+     <MeuBotao conteudo='depois aqui'/>
+
+     <MeuBotao conteudo='e por fim aqui'/>
+    </div>
   )
 }
+
+function MeuBotao(props) {
+
+  return(
+    <button>{props.conteudo}</button>
+  )
+}
+
+
 
 export default App
---
+++ React/meu-projeto-react/src/components/MeuComponente.jsx
@@ -0,0 +1,5 @@
+export default function MeuComponente() {
+    return (
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi ea nisi maiores odit aperiam tenetur illum laborum. Mollitia temporibus deleniti, distinctio, voluptates vero eaque, doloremque in recusandae officiis expedita dignissimos!</p>
+    )
+  }
